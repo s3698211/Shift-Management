de92a15c8d6d715bd08bc5824c20a00632a51156
De Xing Teoh <dexing30100@gmail.com>
2021-05-06 20:19:27 +1000

    recreate app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/layout/Header';
-import Home from './components/pages/Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import CreateShift from './components/pages/CreateShift';
 
 function App() {
   return (
     <div className='App'>
-      <Router>
-        <Header />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/create-shift' exact component={CreateShift} />
-        </Switch>
-      </Router>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
